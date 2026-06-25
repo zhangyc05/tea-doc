41c8168e66b375ed2c831df817fdf15c2b90ce10
zhangyc05 <awef20@163.com>
2026-06-25 16:23:51 +0800

    feat(frontend): T1.3 export UI components

---
+++ frontend/src/components/ui/index.ts
@@ -0,0 +1,53 @@
+export { Button, buttonVariants } from './button'
+export { Badge, badgeVariants } from './badge'
+export { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from './card'
+export { Input } from './input'
+export { Select, SelectGroup, SelectItem } from './select'
+export {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from './table'
+export { Tabs, TabsContent, TabsList, TabsTrigger } from './tabs'
+export {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from './dialog'
+export {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from './sheet'
+export {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from './dropdown-menu'
+export { Tooltip, TooltipContent, TooltipTrigger } from './tooltip'
+export { Popover, PopoverContent, PopoverTrigger } from './popover'
+export { Separator } from './separator'
+export {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from './breadcrumb'
